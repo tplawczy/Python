be3355bedba6e02da56bcc05d56b34de7b1bc8e0
tplawczy <tomasz.plawczyk@gmail.com>
2020-09-25 21:39:23 +0200

    Create tablice.py

---
+++ tablice.py
@@ -0,0 +1,35 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+
+
+
+
+class Tablice:
+    def __init__(self, pocz ):
+        self.pocz = pocz
+        self.array = self.build_array()
+
+    def Tablice(self, pocz):
+        var = len(tablica) = [4]
+        if pocz:
+           return tablica[0]=self.pocz
+        else
+
+
+
+
+
+        tablica = [4]
+
+        return tablica
+
+
+def main():
+
+
+if __name__ == "__main__":
+    main()
