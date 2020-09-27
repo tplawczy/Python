1dce6ea38265d77ed76ea08adbc8a94cb7805827
tplawczy <tomasz.plawczyk@gmail.com>
2020-09-27 21:39:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-# nowy rfr
+# nowy rfr test1
 class Lista():
     i =0
     p =0
