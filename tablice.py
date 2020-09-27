5091ca26fc92a9b893fd7ac3bd075715088775c1
tplawczy <tomasz.plawczyk@gmail.com>
2020-09-27 15:07:52 +0200

    Update tablice.py

--- tablice.py
+++ tablice.py
@@ -4,59 +4,51 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
+class Lista():
+    i = 0
+    p = 0
+    j = 0
 
-
-class Lista:
-    i=0
-    p=0
-    j=0
     def __new__(self, n):
-        self.n=n
+        self.n = n
         list = []
-        for i in range(1,n):
+        for i in range(1, self.n):
             list.append(0)
-            return list
+        return Lista
+
+    def __init__(self, n):
+        self.n = n
+        Lista(n)
 
 
 class Zbiory:
     Lista
+
     def __init__(self, Lista):
         self.Lista = Lista
 
-    def wygenerujKolejny(self):
+    def wygenerujKolejny(self, Lista):
         while Lista.p < Lista.n:
             Lista.i += 1
             Lista.p = 1
             Lista.j = Lista.i
             while Lista.j % 2 == 0:
-                    Lista.j = j / 2
-                    Lista.p += 1
+                Lista.j = j / 2
+                Lista.p += 1
             if Lista.p <= Lista.n:
-                 g=[]
-                 g = 1 - Lista[Lista.p-1]
-                 List[Lista.p-1] = g
+
+                if Lista[(Lista.p) - 1] == 0:
+                    Lista[(Lista.p) - 1] = 1
+                else:
+                    Lista[(Lista.p) - 1] = 0
+
             print(Lista)
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 def main():
     l = Lista(4)
-    e=Zbiory(l)
-    e.wygenerujKolejny()
-
-
+    e = Zbiory(l)
+    e.wygenerujKolejny(l)
 
 
 if __name__ == "__main__":
