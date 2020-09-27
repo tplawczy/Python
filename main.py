52b3ee18de98d7bcc33e8c4ff07cfa0e1218aa9d
tplawczy <tomasz.plawczyk@gmail.com>
2020-09-27 18:53:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,54 +5,36 @@
 
 
 
-class Lista():
-    i = 0
-    p = 0
-    j = 0
-
-    def __new__(self, n):
-        self.n = n
-        list = []
-        for i in range(1, self.n):
-            list.append(0)
-        return Lista
-
-    def __init__(self, n):
-        self.n = n
-        Lista(n)
-
-
 class Zbiory:
-
+    Lista
 
     def __init__(self, Lista):
         self.Lista = Lista
 
-    def wygenerujKolejny(self):
-        while Lista.p < Lista.n:
+    def wygenerujKolejny(self, Lista):
+        while Lista.p <= Lista.n:
+            print(Lista.list)
             Lista.i += 1
             Lista.p = 1
             Lista.j = Lista.i
             while Lista.j % 2 == 0:
-                Lista.j = j / 2
-                Lista.p += 1
+                Lista.j = Lista.j / 2
+                Lista.p = Lista.p + 1
             if Lista.p <= Lista.n:
 
-                if Lista[(Lista.p) - 1] == 0:
-                    Lista[(Lista.p) - 1] = 1
+                if Lista.list[(Lista.p)-1] == 0:
+                    Lista.list[(Lista.p)-1] = 1
                 else:
-                    Lista[(Lista.p) - 1] = 0
+                    Lista.list[(Lista.p)-1] = 0
 
-            print(Lista)
+
 
 
 def main():
     l = Lista(4)
     e = Zbiory(l)
-    print(Lista[Lista.p])
-    e.wygenerujKolejny()
+    e.wygenerujKolejny(l)
 
 
 if __name__ == "__main__":
     main()
-
