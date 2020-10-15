d43969ffcf6d3b941550bfbb93654f6da0651f70
tplawczy <tomasz.plawczyk@gmail.com>
2020-10-15 20:00:29 +0200

    Create dyplom.py

---
+++ dyplom.py
@@ -0,0 +1,47 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+
+
+class Network:
+    def __init__(self,  ):
+        adjacency
+
+
+
+
+
+class Individual:
+    def __init__(self, Network):
+        genom = []
+
+    def generuj(self, genom):
+        genom.append(1)
+
+
+
+
+
+
+
+
+
+
+class Population:
+
+
+
+
+
+def main():
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
